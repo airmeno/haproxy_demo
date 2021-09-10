1fa2b30e074ebb5e910802c4139f280176aa6567
airmeno <meno@list.ru>
2021-09-10 03:12:52 -0700

    haproxy

---
+++ files/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>HTML5</title>
+</head>
+<body>
+	<div align="center">
+    <h2><b>WEB SERVER </b></h2>
+	<h3>It's work!</h3>
+    <font color="green">
+        <?php
+        echo $_SERVER['SERVER_ADDR'];
+        ?>
+    </font>    
+	</div>
+</body>
+</html>
